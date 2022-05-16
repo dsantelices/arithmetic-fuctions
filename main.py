7ee6799a2c53819b503d4168f7dd8c84ac33192e
Daniel Santelices <dsantelices@gmail.com>
2022-05-16 18:45:56 -0400

    split input expression in three components

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 
 
 def process(expression: str):
-    ...
+    expression_cleaned = expression.replace('(', ',').replace(')', '').strip()
+    function_components = expression_cleaned.split(',')
+    function_components.pop(-1)
+    if len(function_components) != 3:
+        print('Error in expression')
+    print(function_components)
+
+
 
 def main():
     test = 'four(times(five()))'
