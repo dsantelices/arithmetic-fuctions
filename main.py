c5d73d7634242657c4a899097da39e2cfb30cad5
Daniel Santelices <dsantelices@gmail.com>
2022-05-16 18:52:32 -0400

    implement functions for numbers and operations

--- main.py
+++ main.py
@@ -1,4 +1,56 @@
 
+def zero():
+    return 0
+
+def one():
+    return 1
+
+def two():
+    return 2
+
+def three():
+    return 3
+
+def four():
+    return 4
+
+def five():
+    return 5
+
+def six():
+    return 6
+
+def seven():
+    return 7
+
+def eight():
+    return 8
+
+def nine():
+    return 9
+
+def plus(val1 :int, val2 :int):
+    return val1 + val2
+
+
+def minus(val1 :int, val2 :int):
+    return val1 - val2
+
+def times(val1 :int, val2 :int):
+    return val1 * val2
+
+def divided_by(val1 :int, val2 :int):
+    # TODO: Check val2 == 0
+    return val1 // val2
+
+
+operations = {
+    'times': times,
+    'plus': plus,
+    'minus': minus,
+    'divided_by': divided_by,
+
+}
 
 def process(expression: str):
     expression_cleaned = expression.replace('(', ',').replace(')', '').strip()
